2dddedc6e4a930da88b172cc5cb3169d17f9d585
BCSBruh <87160862+BCSBruh@users.noreply.github.com>
2022-11-16 17:03:45 -0500

    Put in more advanced methods

--- Project 4/hrCode.py
+++ Project 4/hrCode.py
@@ -35,12 +35,12 @@ r6341 = ngc6341[:, 1]
 colIndex6341 = g6341 - r6341
 
 #Making Plots
-fig, ax = plt.subplots(1,1, layout="constrained")
+fig, ax = plt.subplots(2, 2, layout="constrained")
 
 #Plot 1
 ax[0, 0].scatter(colIndex5024, g5024, s=3, color='black')
-ax[0, 0].plot(0.31, 20, marker='o', markerfacecolor='none', markeredgecolor='r', markeredgewidth=3, ms=20)
-ax[0, 0].axvline(x=0.31, color='r', linestyle='dashed', label='Turnoff Line')
+ax[0, 0].plot(0.28, 20, marker='o', markerfacecolor='none', markeredgecolor='r', markeredgewidth=1.5, ms=20)
+ax[0, 0].axvline(x=0.28, color='r', linestyle='dashed', label='Turnoff Line')
 ax[0, 0].text(0.39, 20, "Turnoff Point", fontsize=10, color='r')
 ax[0, 0].set_xlim(-0, 1.2)
 ax[0, 0].set_ylim(16, 24)
@@ -52,7 +52,7 @@ ax[0, 0].legend(loc='upper right', fontsize=8)
 
 #Plot 2
 ax[0, 1].scatter(colIndex5904, g5904, s=3, color='black')
-ax[0, 1].plot(0.31, 18.2, marker='o', markerfacecolor='none', markeredgecolor='r', markeredgewidth=3, ms=20)
+ax[0, 1].plot(0.31, 18.2, marker='o', markerfacecolor='none', markeredgecolor='r', markeredgewidth=1.5, ms=20)
 ax[0, 1].axvline(x=0.31, color='r', linestyle='dashed', label='Turnoff Line')
 ax[0, 1].text(0.6, 18.2, "Turnoff Point", fontsize=10, color='r')
 ax[0, 1].set_xlim(-0.5, 3)
@@ -64,11 +64,11 @@ ax[0, 1].legend(loc='upper right', fontsize=8)
 
 #Plot 3
 ax[1, 0].scatter(colIndex6229, g6229, s=3, color='black')
-ax[1, 0].plot(0.35, 21.2, marker='o', markerfacecolor='none', markeredgecolor='r', markeredgewidth=3, ms=20)
-ax[1, 0].axvline(x=0.35, color='r', linestyle='dashed', label='Turnoff Line')
+ax[1, 0].plot(0.38, 21.2, marker='o', markerfacecolor='none', markeredgecolor='r', markeredgewidth=1.5, ms=20)
+ax[1, 0].axvline(x=0.38, color='r', linestyle='dashed', label='Turnoff Line')
 ax[1, 0].text(0.45, 21.2, "Turnoff Point", fontsize=10, color='r')
-ax[1, 0].set_xlim(-0.3, 1.3)
-ax[1, 0].set_ylim(18, 24)
+ax[1, 0].set_xlim(0.2, 1)
+ax[1, 0].set_ylim(16, 24.5)
 ax[1, 0].invert_yaxis()
 ax[1, 0].set_xlabel("g-r [mag]")
 ax[1, 0].set_ylabel("g [mag]")
@@ -77,7 +77,7 @@ ax[1, 0].legend(loc='upper right', fontsize=8)
 
 #Plot 4
 ax[1, 1].scatter(colIndex6341, g6341, s=3, color='black')
-ax[1, 1].plot(0.23, 19.5, marker='o', markerfacecolor='none', markeredgecolor='r', markeredgewidth=3, ms=20)
+ax[1, 1].plot(0.23, 19, marker='o', markerfacecolor='none', markeredgecolor='r', markeredgewidth=1.5, ms=20)
 ax[1, 1].axvline(x=0.23, color='r', linestyle='dashed', label='Turnoff Line')
 ax[1, 1].text(0.35, 19.5, "Turnoff Point", fontsize=10, color='r')
 ax[1, 1].set_xlim(-0.2, 1.6)
--- Project 6/main.py
+++ Project 6/main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from lightkurve import search_targetpixelfile as stp
 
 stardes = 'KIC 4282872'
@@ -10,10 +11,22 @@ star_teff = tpf.get_header()[49]
 print('Stellar radius = ', star_rad)
 print('Stellar effective temperature = ', star_teff)
 
-fig, axes = plt.subplots(2, 1, figsize=(6,9), constrained_layout=True)
+fig, axes = plt.subplots(2, 3, figsize=(16, 9), constrained_layout=True)
 
-tpf.plot(ax=axes[0], frame=0)
+tpf.plot(ax=axes[0][0], frame=0)
 lc = tpf.to_lightcurve()
-lc.plot(ax=axes[1], c='k')
+lc.plot(ax=axes[1][0], c='k')
+
+flat_lc = lc.flatten(window_length=401)
+flat_lc.plot(ax=axes[0][1], c='k')
+
+pg = flat_lc.to_periodogram(method='bls', period=np.arange(2, 20, 0.001))
+pg.plot(ax=axes[1][1], c='k')
+periodmax = pg.period_at_max_power
+
+fold_lc = flat_lc.fold(period=periodmax)
+axes[1][2].axis([4.05, 4.45, 0.995, 1.003])
+fold_lc.scatter(ax=axes[1][2], c='k')
+
+#plt.savefig('HelloLightkurvePlot.png', dpi=250)
 plt.show()
-plt.savefig('HelloLightkurvePlot.png', dpi=250)
